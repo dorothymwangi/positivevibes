2aeb55f0fb9c8e4f1f23f62e2c9350eb6b055062
Dorothy Mwangi <dmwangiw@gmail.com>
2019-05-14 10:46:16 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/positivevibes/PositiveVibesApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.positivevibes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositiveVibesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PositiveVibesApplication.class, args);
+	}
+
+}
