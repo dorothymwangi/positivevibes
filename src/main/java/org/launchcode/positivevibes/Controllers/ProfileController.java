06da4c46bf9042aff53e7f046f7f08988b092830
Dorothy Mwangi <dmwangiw@gmail.com>
2019-07-15 11:14:05 -0500

    Added pics

--- src/main/java/org/launchcode/positivevibes/Controllers/ProfileController.java
+++ src/main/java/org/launchcode/positivevibes/Controllers/ProfileController.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Random;
 
 
 @Controller
@@ -34,6 +35,8 @@ public class ProfileController {
         model.addAttribute("user", user);
         model.addAttribute(new Post());
 
+
+        //List of user posts by user Id
         //TODO: Figure out how to get from database by user_id instead of grabbing all users
         List<Post> allPosts = (List<Post>) postDao.findAll();
 
@@ -42,7 +45,33 @@ public class ProfileController {
             if(post.getUser().getId() == user.getId()){
                 userPosts.add(post);
             }
+
         }
+
+        // Generate random quotes
+        //TODO: Figure out how to connect and retrieve random quotes from an API
+
+        String[] arr={"Everything you’ve ever wanted is on the other side of fear. ",
+                "Put your heart, mind, and soul into even your smallest acts. This is the secret of success.",
+                "Perfection is not attainable, but if we chase perfection we can catch excellence. ",
+                "Dwell on the beauty of life. Watch the stars, and see yourself running with them. ",
+                "You are allowed to have fun along the way! ",
+                "Success is something you attract by the person you become - Jim Rohn. ",
+                "Tread softly; breathe peacefully; laugh hysterically – Nelson Mandela. ",
+                "Why fit in when you were born to stand out? – Dr. Seuss ",
+                "One important key to success is self-confidence. An important key to self-confidence is preparation - Arthur Ashe. ",
+                "To know how to wait is the great secret of success – De Maistre. ",
+                "It is our attitude toward life that determines life’s attitude toward us. We get back what we put out – Earl Nightingale. ",
+                "If you run around with 9 losers pretty soon you’ll be the 10th loser - Les Brown. ",
+                "The more you talk about negative things in your life, the more you call them in. Speak victory not defeat – Joel Osteen",
+                "Begin at the beginning… and go on till you come to the end: then stop."};
+
+        Random r=new Random();
+        int randomNumber=r.nextInt(arr.length);
+
+        String quote = arr[randomNumber];
+
+        model.addAttribute("quote", quote);
 
         model.addAttribute("userPosts", userPosts);
 
